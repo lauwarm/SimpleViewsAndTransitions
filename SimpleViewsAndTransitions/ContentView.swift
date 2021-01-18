578bc75af76a364b9d89909d897c4d5ddf142f77
lauwarm <10065855+lauwarm@users.noreply.github.com>
2021-01-18 18:46:28 +0100

    Initial Commit

---
+++ SimpleViewsAndTransitions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleViewsAndTransitions
+//
+//  Created by Fabian on 18.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleViewsAndTransitions/SimpleViewsAndTransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleViewsAndTransitionsApp.swift
+//  SimpleViewsAndTransitions
+//
+//  Created by Fabian on 18.01.21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleViewsAndTransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
